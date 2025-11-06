9d08d995c93ffafcc2cd5aba2d602aa31ffd2ae7
notlucky13166 <lukebeameso1@gmail.com>
2025-11-06 14:01:21 +0000

    Fix registerWebModule polyfill for Netlify build

--- app/src/polyfills/registerWebModule.ts
+++ app/src/polyfills/registerWebModule.ts
@@ -2,15 +2,30 @@ import { Platform } from 'react-native';
 
 type RegisterWebModule = (id: string, loader: () => unknown) => unknown;
 
-interface ExpoModulesCore {
+type ExpoModulesCoreModule = {
+  default?: ExpoModulesCoreModule;
   registerWebModule?: RegisterWebModule;
-}
+};
+
+const ensureRegisterWebModule = (moduleExports: ExpoModulesCoreModule) => {
+  const target = moduleExports.default ?? moduleExports;
+
+  if (typeof target.registerWebModule !== 'function') {
+    const polyfill: RegisterWebModule = (_id, loader) => loader();
+    target.registerWebModule = polyfill;
+  }
+
+  if (!moduleExports.registerWebModule) {
+    moduleExports.registerWebModule = target.registerWebModule;
+  }
+
+  if (moduleExports.default && !moduleExports.default.registerWebModule) {
+    moduleExports.default.registerWebModule = target.registerWebModule;
+  }
+};
 
 if (Platform.OS === 'web') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const expoModulesCore = require('expo-modules-core') as ExpoModulesCore;
-
-  if (typeof expoModulesCore.registerWebModule !== 'function') {
-    expoModulesCore.registerWebModule = (_id, loader) => loader();
-  }
+  const expoModulesCore = require('expo-modules-core') as ExpoModulesCoreModule;
+  ensureRegisterWebModule(expoModulesCore);
 }
